9a22ee9173e4eb046c5be82362c7a5f796d74ea4
Aman chaudhary <chaudharyaman1920@gmail.com>
2025-11-02 10:31:30 +0530

    aman

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // src/config/firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = require("./firebase-db.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
