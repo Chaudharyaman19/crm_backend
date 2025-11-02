5820060c9bc88d4de7b4bf64f2b976a36602d377
Chaudharyaman19 <104840431+Chaudharyaman19@users.noreply.github.com>
2025-11-02 12:52:22 +0530

    Update MongoDB URI to include database name

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const mongo = require("./config/mongo");
 const PORT = process.env.PORT || 4000;
 (async () => {
-  const uri = process.env.MONGO_URI || "mongodb+srv://aarjun8060:Samarpal08%40@cluster0.adxlu1n.mongodb.net/";
+  const uri = process.env.MONGO_URI || "mongodb+srv://aarjun8060:Samarpal08%40@cluster0.adxlu1n.mongodb.net/crm_db";
   await mongo.connect(uri);
   app.listen(PORT, () => console.log("Server running on", PORT));
 })();
